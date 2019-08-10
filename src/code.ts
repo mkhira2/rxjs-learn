11e7f514ae135a8721079e04d2060618f6ba22d3
Kenji Hirabayashi <mkhira2@gmail.com>
2019-08-10 13:01:26 -0500

    creates hot observable

--- src/code.ts
+++ src/code.ts
@@ -1,32 +1,13 @@
 import { Observable } from "rxjs/Observable";
+import { fromEvent } from 'rxjs/Observable/fromEvent';
 
-const observable = Observable.create((observer: any) => {
-    try {
-        observer.next('Hello world')
-        observer.next('How are you?')
-        setInterval(() => {
-            observer.next('I am good')
-        }, 2000)
-    } catch(err) {
-        observer.error(err)
-    }
-});
-
-const observer = observable.subscribe(
-    (x: any) => addItem(x),
-    (error: any) => addItem(error),
-    () => addItem('Completed')
-);
-
-const observer2 = observable.subscribe(
-    (x: any) => addItem(x)
-);
-
-observer.add(observer2)
+const observable = fromEvent(document, 'mousemove')
 
 setTimeout(() => {
-    observer.unsubscribe();
-}, 6000)
+    const subscription = observable.subscribe(
+        (x: any) => addItem(x)
+    )
+}, 2000)
 
 function addItem(val: any) {
     const node = document.createElement('li');
