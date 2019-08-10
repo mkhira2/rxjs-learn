4d31f895a384fcce09fb8eda3ba15d6e79daeb3f
Kenji Hirabayashi <mkhira2@gmail.com>
2019-08-10 12:53:40 -0500

    sets streams, observables, and subscriptions

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,36 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
 
-console.log(Rx);
+const observable = Observable.create((observer: any) => {
+    try {
+        observer.next('Hello world')
+        observer.next('How are you?')
+        setInterval(() => {
+            observer.next('I am good')
+        }, 2000)
+    } catch(err) {
+        observer.error(err)
+    }
+});
+
+const observer = observable.subscribe(
+    (x: any) => addItem(x),
+    (error: any) => addItem(error),
+    () => addItem('Completed')
+);
+
+const observer2 = observable.subscribe(
+    (x: any) => addItem(x)
+);
+
+observer.add(observer2)
+
+setTimeout(() => {
+    observer.unsubscribe();
+}, 6000)
+
+function addItem(val: any) {
+    const node = document.createElement('li');
+    const textNode = document.createTextNode(val);
+    node.appendChild(textNode);
+    document.getElementById('output').appendChild(node);
+}
